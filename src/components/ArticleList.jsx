bc6d3758766903be69ffcf1d4b882cdf5a43de9c
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-05-19 04:11:38 +0300

    feat:added semantic tags

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -3,10 +3,10 @@ import Article from './Article'
 
 function ArticleList({posts}) {
   return (
-    <div>
+    <main>
 {posts.map((post)=>(<Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
 ))}
-    </div>
+    </main>
   )
 }
 
